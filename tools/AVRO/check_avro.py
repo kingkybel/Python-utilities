37074c207f95c3647c2e7f0d32875278e55cb8ac
Dieter Kybelksties <dieter.kybelksties@cityplumbing.co.uk>
2024-07-13 09:18:46 +0100

    added licence headers

--- tools/AVRO/check_avro.py
+++ tools/AVRO/check_avro.py
@@ -1,9 +1,29 @@
 #!/bin/env python3
 
-import argparse
-from os import PathLike
+# Repository:   https://github.com/Python-utilities
+# File Name:    tools/AVRO/check_avro.py
+# Description:  Send AVRO messages on Kafka
+#
+# Copyright (C) 2023 Dieter J Kybelksties <github@kybelksties.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# @date: 2024-07-13
+# @author: Dieter J Kybelksties
 
-from confluent_kafka import Producer, Consumer
+import argparse
 
 from lib.logger import error
 from tools.AVRO.avro_kafka import AvroKafka
